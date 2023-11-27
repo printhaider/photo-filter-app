00a25f07bfbc5327ec28ce57d49a9ab54e6b0bea
printhaider <haider.bajwa@eontariotechu.com>
2023-11-27 18:57:57 -0500

    fixed bugs with seipa button

--- image_filters.py
+++ image_filters.py
@@ -1,38 +1,39 @@
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageOps, ImageEnhance
 
-def apply_sepia(image):
-    """
-    Apply a sepia filter to the given image.
-    """
+def apply_black_and_white(image):
+    return image.convert("L")
+
+def apply_sepia(image, intensity=1):
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
     width, height = image.size
-    pixels = image.load()  # create the pixel map
+    pixels = image.load()
 
     for py in range(height):
         for px in range(width):
             r, g, b = image.getpixel((px, py))
 
-            # Calculate new values
-            tr = int(0.393 * r + 0.769 * g + 0.189 * b)
-            tg = int(0.349 * r + 0.686 * g + 0.168 * b)
-            tb = int(0.272 * r + 0.534 * g + 0.131 * b)
+            tr = int((0.393 * r + 0.769 * g + 0.189 * b) * intensity)
+            tg = int((0.349 * r + 0.686 * g + 0.168 * b) * intensity)
+            tb = int((0.272 * r + 0.534 * g + 0.131 * b) * intensity)
 
-            # Reassign new values keeping in mind the maximum value
-            pixels[px, py] = (min(tr, 255), min(tg, 255), min(tb, 255))
+            tr = min(255, max(0, tr))
+            tg = min(255, max(0, tg))
+            tb = min(255, max(0, tb))
+
+            pixels[px, py] = (tr, tg, tb)
 
     return image
 
+
 def apply_vintage(image):
-    """
-    Apply a vintage filter to the given image.
-    """
-    # You can adjust these values for different effects
     converter = ImageEnhance.Color(image)
-    image = converter.enhance(0.7)  # Adjust color balance
+    image = converter.enhance(0.7)
 
     converter = ImageEnhance.Brightness(image)
-    image = converter.enhance(1.3)  # Adjust brightness
+    image = converter.enhance(1.3)
 
     converter = ImageEnhance.Contrast(image)
-    image = converter.enhance(1.5)  # Adjust contrast
+    image = converter.enhance(1.5)
 
     return image
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
-import image_filters  # This is the file with your filter functions
+import image_filters
 
 def load_image():
     global img, tk_img, history
+    hide_sepia_slider()  # Hide sepia slider when a new image is loaded
     file_path = filedialog.askopenfilename()
     if file_path:
         img = Image.open(file_path)
@@ -12,19 +13,31 @@ def load_image():
         canvas.create_image(20, 20, anchor='nw', image=tk_img)
         history = [img.copy()]
 
-def apply_filter(filter_function):
+def apply_filter(filter_function, *args):
     global img, tk_img, history
+    hide_sepia_slider()  # Hide sepia slider when applying any filter
     if img:
-        history.append(img.copy())  # Save current state before applying the filter
-        img = filter_function(img)
+        history.append(img.copy())
+        img = filter_function(img, *args)
         tk_img = ImageTk.PhotoImage(img)
         canvas.create_image(20, 20, anchor='nw', image=tk_img)
 
+def show_sepia_slider():
+    sepia_intensity_slider.pack()
+    apply_sepia_with_intensity()
+
+def hide_sepia_slider():
+    sepia_intensity_slider.pack_forget()
+
+def apply_sepia_with_intensity(*args):
+    apply_filter(image_filters.apply_sepia, sepia_intensity.get())
+
 def redo():
     global img, tk_img, history
-    if len(history) > 1:  # Check if there is a state to revert to
-        history.pop()  # Remove the current state
-        img = history[-1]  # Revert to the previous state
+    hide_sepia_slider()  # Hide sepia slider when redoing
+    if len(history) > 1:
+        history.pop()
+        img = history[-1].copy()
         tk_img = ImageTk.PhotoImage(img)
         canvas.create_image(20, 20, anchor='nw', image=tk_img)
 
@@ -33,7 +46,7 @@ root.title("Image Filter App")
 
 img = None
 tk_img = None
-history = []  # Initialize history
+history = []
 
 canvas = tk.Canvas(root, width=600, height=400)
 canvas.pack()
@@ -41,13 +54,20 @@ canvas.pack()
 load_button = tk.Button(root, text="Load Image", command=load_image)
 load_button.pack()
 
-sepia_button = tk.Button(root, text="Apply Sepia", command=lambda: apply_filter(image_filters.apply_sepia))
+bw_button = tk.Button(root, text="Black and White", command=lambda: apply_filter(image_filters.apply_black_and_white))
+bw_button.pack()
+
+sepia_button = tk.Button(root, text="Sepia", command=show_sepia_slider)
 sepia_button.pack()
 
-vintage_button = tk.Button(root, text="Apply Vintage", command=lambda: apply_filter(image_filters.apply_vintage))
+vintage_button = tk.Button(root, text="Vintage", command=lambda: apply_filter(image_filters.apply_vintage))
 vintage_button.pack()
 
 redo_button = tk.Button(root, text="Redo", command=redo)
 redo_button.pack()
+
+# Sepia intensity slider (initially hidden)
+sepia_intensity = tk.DoubleVar(value=1)
+sepia_intensity_slider = tk.Scale(root, from_=0, to=1, resolution=0.01, orient='horizontal', label='Sepia Intensity', variable=sepia_intensity, command=apply_sepia_with_intensity)
 
 root.mainloop()
